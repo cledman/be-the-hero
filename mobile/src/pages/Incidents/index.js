9a4eebbb69fa7514f8f4d701e5d0d30b56947264
Cledson Lopes <cledman@gmail.com>
2020-03-27 16:34:05 -0300

    Mobile da aplicação  concluído

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,9 @@
+import React from './node_modules/react'
+import { View } from 'react-native'
+import logoImg from '../../assets/logo.png'
+
+export default function Incidents(){
+    return(
+        <View />
+    )
+}
